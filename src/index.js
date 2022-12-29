264c026ab60476a29b0bbae8cfba754c48472eda
unknown <4ndrey.tkachenko@gmail.com>
2022-12-29 16:12:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
+// import ReactDOM from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import { store } from './redux/store';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// );
